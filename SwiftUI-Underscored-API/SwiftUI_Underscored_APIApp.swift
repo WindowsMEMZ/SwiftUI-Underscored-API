cc0bff97a5568d309dd505ce70527219dd7bbe47
WindowsMEMZ <memz1@darock.top>
2025-01-23 05:30:52 +0800

    Initial Commit

---
+++ SwiftUI-Underscored-API/SwiftUI_Underscored_APIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_Underscored_APIApp.swift
+//  SwiftUI-Underscored-API
+//
+//  Created by Mark Chan on 2025/1/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_Underscored_APIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
